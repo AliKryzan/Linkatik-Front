fac1a308045a26add5921334729c993991dbd3ca
X_V.neer <xv.neer@gmail.com>
2025-01-01 14:24:48 +0200

    handle applying theme

--- src/components/bio-pages/appearane/themes.jsx
+++ src/components/bio-pages/appearane/themes.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import { Group, Radio, Stack, Text } from "@mantine/core"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useTranslation } from "react-i18next"
+import { useParams } from "react-router-dom"
 
+import { PutUpdateBioPage } from "../../../services/utils"
 import Error from "../../common/error"
 import Loader from "../../common/loader"
 import CreateCustomTheme from "./create-custom-theme"
@@ -12,13 +15,32 @@ const Themes = ({ bioPageThemesQuery, data }) => {
   const { status, data: themes } = bioPageThemesQuery
   const [value, setValue] = useState(data.bio_page_theme.id + "")
 
+  // handle change theme
+  const { path, id } = useParams()
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: async ({ value }) => {
+      return await PutUpdateBioPage({
+        id,
+        data: { bio_page_theme_id: value },
+      })
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["bio-page-theme-preview", path] })
+    },
+  })
   if (status === "pending") return <Loader />
   if (status === "error") return <Error />
   return (
     <>
       <Stack gap={"xl"}>
         <Text size="xl">{t("bioPages.appearance.themes.title")}</Text>
-        <Radio.Group value={value} onChange={setValue}>
+        <Radio.Group
+          value={value}
+          onChange={(value) => {
+            setValue(value)
+            mutate({ value: value })
+          }}>
           <Group px={{ base: "md", md: "lg" }} gap={"lg"}>
             <Stack>
               <Radio.Card className="theme-preview" radius="md" value={"custom"}>
