00d38452bbff4f455a44771866255a8e47d5250d
OsamaHIma <osamahima018@gmail.com>
2025-02-25 00:56:32 +0200

    push

--- src/components/bio-pages/Create-chooce-avatar.jsx
+++ src/components/bio-pages/Create-chooce-avatar.jsx
@@ -1,44 +1,32 @@
-import React from 'react';
-import { Box, Image, Group } from "@mantine/core";
-import { groupAvatar } from '@/assets';
-import Loader from '../common/loader';
-import { setBioImage , setImage_type } from '../../store/General-variables/General-variables';
-import { useDispatch , useSelector} from "react-redux"
+import React from "react"
+import { groupAvatar } from "@/assets"
+import { Box, Group, Image } from "@mantine/core"
+import { useDispatch, useSelector } from "react-redux"
 
+import { setBioImage, setImage_type } from "../../store/General-variables/General-variables"
+import Loader from "../common/loader"
 
-function CreateChooseAvatar({setActiveAvatar,setModalOneOpen}) {
-
-  const {image_type } = useSelector((state) => state.GeneralSlice)
+function CreateChooseAvatar({ setActiveAvatar, setModalOneOpen }) {
+  const { image_type } = useSelector((state) => state.GeneralSlice)
 
   const dispatch = useDispatch()
 
   const handleClick = async (index) => {
-    const selectedImage = groupAvatar[index];
-    console.log(`Selected image: ${selectedImage}`);
+    const selectedImage = groupAvatar[index]
+    console.log(`Selected image: ${selectedImage}`)
     setActiveAvatar(index)
     dispatch(setBioImage(groupAvatar[index]))
-     dispatch(setImage_type('avatar'))
+    dispatch(setImage_type("avatar"))
     setModalOneOpen(false)
-  };
+  }
 
   return (
     <Group style={{ display: "grid", gridTemplateColumns: "repeat(6, 1fr)", gap: "16px" }}>
       {groupAvatar.map((item, index) => (
         <Box
           key={index}
-          onClick={() => handleClick(index)} 
-          style={{
-            width: "100px",
-            height: "100px",
-            border: "2px solid #8938b2",
-            overflow: "hidden",
-            borderRadius: "50%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            cursor: "pointer",
-          }}
-        >
+          onClick={() => handleClick(index)}
+          className="flex h-[100px] w-[100px] cursor-pointer items-center justify-center overflow-hidden rounded-full border-2 border-[#8938b2] transition-shadow hover:shadow-lg">
           <img
             src={item.image}
             alt={`Avatar ${index + 1}`}
@@ -51,7 +39,7 @@ function CreateChooseAvatar({setActiveAvatar,setModalOneOpen}) {
         </Box>
       ))}
     </Group>
-  );
+  )
 }
 
-export default CreateChooseAvatar;
+export default CreateChooseAvatar
