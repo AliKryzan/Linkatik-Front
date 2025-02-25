09415e192984bc5c193884f8ccee816a08c11309
OsamaHIma <osamahima018@gmail.com>
2025-02-26 01:02:36 +0200

    push

--- src/components/bio-pages/appearance/font.jsx
+++ src/components/bio-pages/appearance/font.jsx
@@ -1,15 +1,15 @@
+import { useEffect } from "react"
 import { Box, ColorInput, Group, Select, Space, Stack, Text, useMantineColorScheme } from "@mantine/core"
 import { LetterTextIcon } from "lucide-react"
-import { useEffect } from "react"
 import { Controller } from "react-hook-form"
 import { useTranslation } from "react-i18next"
 import { useSelector } from "react-redux"
 
 const Font = ({ form }) => {
   const { t } = useTranslation()
-  const { control } = form;
+  const { control } = form
   const { colorScheme } = useMantineColorScheme()
-  const { main_button_color , main_text_color} = useSelector((state) => state.GeneralSlice)
+  const { main_button_color, main_text_color } = useSelector((state) => state.GeneralSlice)
   return (
     <>
       <Space />
@@ -24,38 +24,35 @@ const Font = ({ form }) => {
                 <Select
                   label={t("bioPages.appearance.themes.style")}
                   classNames={{
-                    input:"!h-15 !ps-13",
-
+                    input: "!h-15 !ps-13",
                   }}
                   data={[
                     { value: "Tajawal", label: "Tajawal" },
                     { value: "light", label: t("bioPages.appearance.themes.light") },
                   ]}
-                  leftSection={<div className="bg-gray-200 text-lg text-black rounded-xl py-2 px-3 ms-4">Aa</div>}
+                  leftSection={
+                    <div className="ms-4 rounded-xl bg-gray-200 px-3 py-2 text-lg text-black">Aa</div>
+                  }
                   {...field}
                   onChange={(value) => field.onChange(value)}
                 />
               )}
             />
-              <Controller
+            <Controller
               key={"bio_link.text_color"}
               name="bio_link.text_color"
               control={control}
               render={({ field }) => {
                 useEffect(() => {
                   if (!field.value) {
-                    field.onChange(main_text_color);
+                    field.onChange(main_text_color)
                   }
-                }, [ main_text_color]);
+                }, [main_text_color])
                 return (
-                  <Group gap={"sm"}>
-                    <Box
-                      style={{
-                        width: "50px",
-                        height: "50px",
-                        background: form.watch("bio_link.text_color"),
-                        borderRadius: "var(--mantine-radius-md)",
-                      }}></Box>
+                  <div className="grid gap-0">
+                    <Text size="sm" mb="xs">
+                      {t("bioPages.appearance.themes.form.text-color")}
+                    </Text>
                     <Box
                       style={{
                         borderRadius: "var(--mantine-radius-md)",
@@ -65,16 +62,14 @@ const Font = ({ form }) => {
                       <ColorInput
                         size="sm"
                         w={200}
-                        //leftSection={"HEXA"}
                         format="hexa"
                         variant="unstyled"
-                        label={t("bioPages.appearance.themes.form.text-color")}
-                        defaultValue={field?.value || main_text_color} 
-                        onChange={(color) => field.onChange(color)} 
+                        defaultValue={field?.value || main_text_color}
+                        onChange={(color) => field.onChange(color)}
                         {...field}
                       />
                     </Box>
-                  </Group>
+                  </div>
                 )
               }}
             />
